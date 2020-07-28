5cbcb623565fcc446faca2127a429d8d550db21b
albinoty <albinotnoty@gmail.com>
2020-07-28 21:07:43 +0200

    Notion Data

---
+++ src/components/DataBinding.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>V-bind</h2>
+    <a v-bind:href="linkFb">Lien vers fb</a>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'DataBinding',
+  data() {
+    return {
+      linkFb: 'https://facebook.com',
+    }
+  },
+  components: {
+    
+  },
+  methods: {
+   
+  }
+}
+</script>
