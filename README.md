2673bfbf3f0855b38202e50bf1aa4cf4c5b7db71
albinoty <albinotnoty@gmail.com>
2020-07-28 21:06:39 +0200

    First Commit, initialisation du projet

---
+++ README.md
@@ -0,0 +1,24 @@
+# Tuto VueJs
+
+Le repo de permet de voir comment fonctionne VueJs via un cours
+
+
+## Project setup
+```BASH
+npm install
+```
+
+### Compiles and hot-reloads for development
+```BASH
+npm run serve
+```
+
+### Compiles and minifies for production
+```BASH
+npm run build
+```
+
+### Lints and fixes files
+```BASH
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Vue</h1> 
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  },
+  methods: {
+   
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
